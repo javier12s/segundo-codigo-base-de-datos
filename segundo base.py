a2d63c8f434a3f4468a7d5283c6d4b8aeb9901b0
javier12s <151806272+javier12s@users.noreply.github.com>
2024-09-07 16:48:37 -0600

    Add files via upload

---
+++ segundo base.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+nuevos indices = ['A', 'B', 'C' , 'D', 'E'] 
+arreglo numpy = [10, 20, 30, 40, 50]
+
+edades_ diccionario = pd.Series (arreglo numpy, index=nuevos _indices)
+edades_ diccionario
